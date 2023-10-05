11468e33453851e229cacee77045d7add6438cb6
Perizat <perizatse@mail.ru>
2023-10-05 07:42:12 +0600

    first commit

---
+++ src/Magzhan_java.java
@@ -0,0 +1,55 @@
+public class Magzhan_java {
+    public static void main(String[] args) {
+System.out.println("Hello world");// моя первая написанная программа.
+//однострочный комментарий
+        /*
+        первая строка
+        вторая строка
+        многострочный комментарий
+         */
+String flowersName = "Roses";
+int flowersNumber = 101;
+System.out.println(flowersName);
+System.out.println(flowersNumber);
+        int a = 10;
+        int b = 15;
+        System.out.println(a+b);
+        int x = 20;
+        int y = 10;
+        int z = x+y;
+        System.out.println(z);
+        int q = 1, w = 2, e = 3, r = 4;
+        System.out.println(q+w+e+r);
+        //переменная с разными значениями
+        int s =10, d=10, f=10, g=10;
+        System.out.println(s+d+f+g);
+        //один тип с одинаковыми значениями
+double myDouble;
+myDouble=2.5;
+System.out.println(myDouble);
+//Объявление дабла и его дальнейшая инициализация
+byte Ba=100;
+short sh=101;
+int Black=10001;
+long White=1000000001;
+double yourDouble=3.5;
+float myFloat=3.6f;
+boolean Tiktak=true;
+char Ch='S';
+String Lays= "Hello Kitty";
+//примеры переменных
+int Chacha=100;
+long Bo=Chacha;
+        //пример расширяющегося приведения
+        int Fu=100;
+        byte As= (byte)Fu;
+//пример сужающегося приведения
+        final double GRAVITY= 9.8;
+        final double PI=3.14;
+        final String city="Almaty";
+        //пример констант
+
+
+    }
+}
+
