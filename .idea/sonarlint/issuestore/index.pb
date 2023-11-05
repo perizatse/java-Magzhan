df8d9b6d26b4f808b66aeddb38cb417fdfd487b0
Perizat <perizatse@mail.ru>
2023-11-05 13:33:13 +0600

    Extra tasks

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,7 +1,9 @@
 
-E
-src/Magzhan_java.java,2\6\265c01bba7a5f9adb8c0bfe81dbd81c2b1a23e02
-D
-src/Perizatjava.java,7\e\7e5a17024176497a21040ea1bd711291c9325418
 >
 src/Loops.java,7\e\7e990ec1fd29c5c32d90e8c96a8fa39b9fd50877
+A
+src/Practice.java,a\c\ac2ef14ebcaa2a21e6d3b8b395a3e231207b6b99
+A
+src/Homework.java,a\2\a26134c50e541b93482e332f99f7aec24a75dd86
+?
+src/Arrays.java,6\3\633db0627e25cab6910ec2aedfd3bac52672c1c7
---
+++ src/Extra.java
@@ -0,0 +1,94 @@
+import java.util.Scanner;
+
+public class Extra {
+    public static void main(String[] args) {
+//Напишите программу, которая проверяет, является ли заданное целое число четным или нечетным, и выводит соответствующее сообщение.
+
+        int q = 155;
+        if (q % 2 == 0) {
+            System.out.println("Число" + " " + q + " = четное");
+        } else {
+            System.out.println("Число" + " " + q + " = нечетное");
+        }
+//Напишите программу, которая сравнивает два целых числа и определяет, какое из них больше.
+
+        int a1 = 40;
+        int b1 = 35;
+        if (a1 > b1) {
+            System.out.println("А больше, чем B");
+        } else {
+            System.out.println(" A меньше, чем B");
+        }
+//Напишите программу, которая определяет, является ли заданный год високосным.
+        int year = 2013;
+        if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+//Напишите программу, которая принимает номер дня недели (1 - Понедельник, 2 - Вторник и так далее) и выводит название дня недели.
+        int numday = 1;
+        switch (numday) {
+            case 1:
+                System.out.println("Сегодня понедельник");
+                break;
+            case 2:
+                System.out.println("Сегодня вторник");
+                break;
+            case 3:
+                System.out.println("Сегодня среда");
+                break;
+            case 4:
+                System.out.println("Сегодня четверг");
+                break;
+            case 5:
+                System.out.println("Сегодня пятница");
+                break;
+            case 6:
+                System.out.println("Сегодня суббота");
+                break;
+            case 7:
+                System.out.println("Сегодня воскресенье");
+                break;
+            default:
+                System.out.println("Неверно указан день недели");
+                break;
+        }
+        int x = 10;
+        int y = 20;
+
+        boolean OneQuarter = (x > 0 && y > 0);
+        boolean TwoQuarter = (x < 0 && y > 0);
+        boolean ThreeQuarter = (x < 0 && y < 0);
+        boolean FourQuarter = (x > 0 && y < 0);
+
+        if (OneQuarter) {
+            System.out.println("1");
+        } else if (TwoQuarter) {
+            System.out.println("2");
+        } else if (ThreeQuarter) {
+            System.out.println("3");
+        } else if (FourQuarter) {
+            System.out.println("4");
+        }
+        int a = 20;
+
+        int b = 10;
+
+        int c = 30;
+
+
+        if ((a == b) && (b == c))
+
+            System.out.println("Треугольник равносторонний");
+
+        else if ((a == b) || (a == c) || (b == c))
+
+            System.out.println("Треугольник равнобедренный");
+
+        else
+
+            System.out.println("Треугольник разносторонний");
+
+    }
+}
